fd430004c5e011427afb19ebd29498d8d3e1df7e
Keien Ohta <keien.ohta@gmail.com>
2013-08-22 12:54:04 -0700

    Added seeds.rb back in

---
+++ db/seeds.rb
@@ -0,0 +1,20 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times { FactoryGirl.create(:member) }
+10.times { FactoryGirl.create(:committee) }
+5.times { FactoryGirl.create(:committee_member_type) }
+
+Member.all.each do |member|
+  committee_member = member.committee_members.create!(
+    committee_id: Committee.first(offset: rand(Committee.count)).id,
+    committee_member_type_id: CommitteeMemberType.first(offset: rand(CommitteeMemberType.count)).id,
+  )
+end
+
+statuses = [:available, :attending, :attended, :late, :missed, :make_up]
+statuses.each { |status| Status.create!(name: status) }
