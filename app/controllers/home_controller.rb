877a6d5cb97ac871cf8d9000f9792091089c1356
Keien Ohta <keienohta@gmail.com>
2013-10-06 01:36:09 -0700

    Changed home to show only tabling for the coming week

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
 
   def home
-    @tabling_slots = current_member.tabling_slots
+    @tabling_slots = current_member.tabling_slots.where(
+      "start_time >= :start", start: tabling_start
+    )
   end
 
 end
