7d53eec48c59f08744e3fd9ad444a3742383d737
david liu <davidbliu@gmail.com>
2014-01-21 01:05:55 -0800

    lkj

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  CLIENT_ID = "296875203176.apps.googleusercontent.com"
-  CLIENT_SECRET = "Mf0mtCVzYBKRXvISeWrJw2Dz"
+  # CLIENT_ID = "296875203176.apps.googleusercontent.com"
+  # CLIENT_SECRET = "Mf0mtCVzYBKRXvISeWrJw2Dz"
   # davids
-  # CLIENT_ID = "158145275272-lj3m0j741dj9fp50rticq48vtrfu59jj.apps.googleusercontent.com"
-  # CLIENT_SECRET = "XvvJaFO2t2lD0mEzNdya6NgT"
+  CLIENT_ID = "158145275272-lj3m0j741dj9fp50rticq48vtrfu59jj.apps.googleusercontent.com"
+  CLIENT_SECRET = "XvvJaFO2t2lD0mEzNdya6NgT"
   provider :google_oauth2, CLIENT_ID, CLIENT_SECRET, {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email
