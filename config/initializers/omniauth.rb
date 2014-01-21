076f288055a253087013895a68dd4bd34e519234
david liu <davidbliu@gmail.com>
2014-01-20 23:43:33 -0800

    auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,15 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  CLIENT_ID = "296875203176.apps.googleusercontent.com"
-  CLIENT_SECRET = "Mf0mtCVzYBKRXvISeWrJw2Dz"
+  # CLIENT_ID = "296875203176.apps.googleusercontent.com"
+  # CLIENT_SECRET = "Mf0mtCVzYBKRXvISeWrJw2Dz"
+  # davids
+  CLIENT_ID = "158145275272-lj3m0j741dj9fp50rticq48vtrfu59jj.apps.googleusercontent.com"
+  CLIENT_SECRET = "XvvJaFO2t2lD0mEzNdya6NgT"
   provider :google_oauth2, CLIENT_ID, CLIENT_SECRET, {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email
             https://www.googleapis.com/auth/calendar
             https://www.googleapis.com/auth/plus.login',
-    redirect_uri:'http://localhost/auth/google_oauth2/callback'
+    redirect_uri:'http://ucbpblmp-staging.herokuapp.com/auth/google_oauth2/callback'
+    # redirect_uri: ''
   }
 end
