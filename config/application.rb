2eda08c8966244f6b0581863160619209f3978f0
david liu <davidbliu@gmail.com>
2014-01-21 00:41:31 -0800

    reverted precompile to true

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,5 @@ module Ucbpblmembersportal
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     config.action_controller.include_all_helpers
-    config.assets.initialize_on_precompile = false
   end
 end
