89cfab5612116519ee1974805e0296f586c458a2
david liu <davidbliu@gmail.com>
2014-01-21 19:47:47 -0800

    all int

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,12 +21,19 @@ class SessionsController < ApplicationController
       },
       headers: { 'Content-Type' => 'application/json' }
     )
-    puts result.data
-    if result.data.error
-      puts "there was an ERROR"
-      puts result.data.error
-    else
-      puts "there was no ERROR you should go on"
+    all_events = process_google_events(result.data.items)
+    all_events.each do |e|
+      event = Event.new
+      if Event.where(google_id: e[:id]).length != 0
+        event = Event.where(google_id: e[:id]).first
+      end
+      if e[:start_time] and e[:end_time]
+        event.google_id = e[:id]
+        event.start_time = e[:start_time]
+        event.end_time = e[:end_time]
+        event.name = e[:summary]
+        event.save
+      end
     end
   end
 
@@ -65,6 +72,36 @@ class SessionsController < ApplicationController
     cookies[:remember_token] = nil
     current_member = nil;
     redirect_to "https://accounts.google.com/logout"
+  end
+
+
+
+
+  def process_google_events(events, options={})
+    results = []
+    events.each do |event|
+      start_time = google_datetime_fix(event.start)
+      end_time = google_datetime_fix(event.end)
+
+      if options[:this_week]
+        start_time = this_week(start_time)
+        end_time = this_week(end_time)
+      end
+
+      results << {
+        id: event.id,
+        summary: event.summary,
+        start_time: start_time,
+        end_time: end_time,
+      }
+    end
+
+    return results.sort_by {|event| event[:start_time]}
+  end
+
+  def google_datetime_fix(datetime)
+    time_string = datetime.date_time ? datetime.date_time.to_s : datetime.date.to_s
+    DateTime.parse(time_string)
   end
 
 end
