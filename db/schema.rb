76c58751a57f6b60994d02303ff7fbd2192dc282
Keien Ohta <keienohta@gmail.com>
2014-02-21 15:39:18 -0800

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -173,9 +173,8 @@ ActiveRecord::Schema.define(:version => 20140203004620) do
 
   create_table "deliberations", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
-    t.string   "can_view_graph"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "event_members", :force => true do |t|
